6a6244b82309a8c428348b0c3f23fa277e0c2dfc
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-08-03 00:25:56 +0600

    u

--- pages/tf.js
+++ pages/tf.js
@@ -1,166 +1,198 @@
 import { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
-import styles from "../styles/Home.module.css";
 
-export default function ObjectDetection() {
-    const videoEle = useRef(null);
-    const canvasEle = useRef(null);
-    const [objectDetector, setObjectDetectors] = useState(null);
-    const [detectedObjects, setDetectedObjects] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [shouldIntervalStart, setShouldIntervalStart] = useState(false);
+const ObjectDetection = () => {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [objectDetector, setObjectDetector] = useState(null);
+  const [detectedObjects, setDetectedObjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [snapshot, setSnapshot] = useState(null);
 
-    const draw = (ctx, objects) => {
-        // Clear the canvas
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  const loadObjectDetector = async () => {
+    try {
+      const model = await cocossd.load();
+      setObjectDetector(model);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error loading model", error);
+    }
+  };
 
-        // // Draw the video frame
-        ctx.drawImage(videoEle.current, 0, 0, ctx.canvas.width, ctx.canvas.height);
-        for (let i = 0; i < objects.length; i++) {
-            const bbox = objects[i].bbox;
-
-            // Draw the bounding box
-            ctx.beginPath();
-            // ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
-            // should be half the size of the bbox
-            ctx.rect(bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[2] / 2, bbox[3] / 2);
-            ctx.strokeStyle = "green";
-            ctx.lineWidth = 2;
-            ctx.stroke();
-            ctx.closePath();
-
-            // Draw the label background
-            ctx.fillStyle = "red";
-            const textWidth = ctx.measureText(objects[i].class).width;
-            ctx.fillRect(bbox[0], bbox[1] - 20, textWidth + 8, 20);
-
-            // Write the label text
-            ctx.font = "16px Arial";
-            ctx.fillStyle = "black";
-            let text = objects[i].class + " - " + Math.round(objects[i].score * 100) + "%";
-            ctx.fillText(text, bbox[0] + 4, bbox[1] - 4);
-        }
-    };
-
-    const startDetecting = async (
-        frameSkipCount = 1, // Default value is 1, meaning no frame skipping
-        frameCountThreshold = 100 // Default threshold to reset frameCount
-    ) => {
-        if (!objectDetector) return;
-
-        let frameCount = 0;
-
-        const detectFrame = async () => {
-            if (frameCount % frameSkipCount === 0) {
-                const image = tf.browser.fromPixels(videoEle.current);
-                const predictions = await objectDetector.detect(image);
-
-                setDetectedObjects(predictions);
-                if (canvasEle.current) {
-                    draw(canvasEle.current.getContext("2d"), predictions);
-                }
-
-                tf.dispose(image); // Dispose the intermediate tensor to free up memory
-            }
-
-            frameCount++;
-            if (frameCount >= frameCountThreshold) {
-                frameCount = 0; // Reset frameCount when it reaches the threshold
-            }
-            requestAnimationFrame(detectFrame); // Continue to the next frame
-        };
-
-        // Start the detection loop by calling the initial frame detection
-        requestAnimationFrame(detectFrame);
-    };
-
-    const loadOCRModel = async () => {
-        try {
-            const model = await cocossd.load();
-            setObjectDetectors(model);
-            setIsLoading(false);
-        } catch (error) {
-            console.error("Error loading model", error);
-        }
-    };
-
-    useEffect(() => {
-        loadOCRModel();
-    }, []);
+  useEffect(() => {
+    loadObjectDetector();
+  }, []);
 
 
-    const handleVideoStream = (stream) => {
-        videoEle.current.srcObject = stream;
-        videoEle.current.onloadedmetadata = () => {
-            videoEle.current.play();
-            startDetecting();
-        };
-    };
+  const handleSnapshot = async () => {
+    setDetectedObjects([]); // Clear previous detections
+    if (videoRef.current && canvasRef.current) {
 
-    const handleVideoError = (error) => {
-        console.error("Error accessing video stream: ", error);
-    };
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    useEffect(() => {
-        if (detectedObjects && detectedObjects.length > 0 && !shouldIntervalStart) {
-            setShouldIntervalStart(true);
-        }
-    }, [detectedObjects]);
+      const tempCanvus = document.createElement("canvas");
+      const video = videoRef.current;
+      tempCanvus.width = canvas.width;
+      tempCanvus.height = canvas.height;
+      tempCanvus.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+      ctx.drawImage(tempCanvus, 0, 0, canvas.width, canvas.height);
+      const image = tf.browser.fromPixels(tempCanvus);
+      if (!objectDetector) return;
+      const predictions = await objectDetector.detect(image);
 
-    // if seeing an object in the screen for the first time, send a notification and wait for 5 seconds, then see if there is still an object in the screen, if there is, send another notification and so on
+      // Draw the class name and probability for each detected object
+      predictions.forEach((obj) => {
+        ctx.beginPath();
+        ctx.lineWidth = "2";
+        ctx.strokeStyle = "green";
+        ctx.fillStyle = "green";
+        ctx.font = "10px Arial";
+        ctx.fillText(`${obj.class} (${Math.round(obj.score * 100)}%)`, obj.bbox[0], obj.bbox[1] - 5);
+        const x = obj.bbox[0];
+        const y = obj.bbox[1];
+        const width = obj.bbox[2];
+        const height = obj.bbox[3];
+        console.log('x', x, 'y', y, 'width', width, 'height', height);
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+      });
 
-    const sendInstructions = (object) => {
-        if (object) {
-            console.log("An object is detected", object);
-        }
-        else {
-            console.log("no object detected");
-        }
+      tf.dispose(image); // Dispose the intermediate tensor to free up memory
+    }
+  };
+
+
+  let mediaStream = null;
+
+  const startCamera = async (facingMode) => {
+    try {
+      const constraints = { video: { facingMode: facingMode } };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      videoRef.current.srcObject = stream;
+      mediaStream = stream;
+    } catch (error) {
+      console.error('Error accessing webcam:', error);
+    }
+  };
+
+  const tryCameraAccess = async () => {
+    try {
+      await startCamera('environment'); // Try the back camera (environment facing mode)
+    } catch (error) {
+      // If the back camera fails, try the front camera (user facing mode)
+      await startCamera('user');
+    }
+  };
+
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      tryCameraAccess();
     }
 
+    // Clean up the media stream when the component unmounts
+    return () => {
+      if (mediaStream) {
+        mediaStream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
+  return (
+    <div className="camera-container">
+      <div className="video-container">
+        <video ref={videoRef} autoPlay playsInline muted />
+      </div>
+      <div className="preview-container">
+        {isLoading && <p>Please wait while the model is loading...</p>}
+        <canvas ref={canvasRef} />
+      </div>
 
-
-    useEffect(() => {
-        if (setShouldIntervalStart) {
-            console.log("detectedObjects", detectedObjects);
-            const interval = setInterval(() => {
-                if (detectedObjects && detectedObjects.length > 0) { sendInstructions(detectedObjects[0].class); }
-                // else sendInstructions(null);
-
-            }, 5000);
+      <button className="snapshot-btn" onClick={handleSnapshot}>
+        <div className="snapshot-btn__inner" />
+      </button>
+      <style jsx>{`
+        .camera-container {
+          display: flex;
+          width: 100%;
+          height: 100vh;
         }
-    }, [shouldIntervalStart]);
 
-    useEffect(() => {
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then(handleVideoStream)
-            .catch(handleVideoError);
-    }, [videoEle.current]);
+        .video-container,
+        .preview-container {
+          width: 50%;
+          height: 100%;
+          overflow: hidden;
+          background-color: black;
+          color: white;
+        }
 
-    return (
-        <>
-            <div>
-                {isLoading && (
-                    <div>
-                        <p>Please wait while the model is loading...</p>
-                    </div>
-                )}
-            </div>
-            <div className={styles.container} >
-                <video
-                    ref={videoEle}
-                    width={300}
-                    height={300}
-                />
-                <canvas
-                    ref={canvasEle}
-                    className={styles.canvas}
-                    width={300}
-                    height={300}
-                />
-            </div>
-        </>
-    );
-}
+        .video-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        video {
+          width: 100%;
+        }
+
+        .preview-container {
+          display: flex;
+          flex-direction: column;
+          padding: 1rem;
+        }
+
+        canvas {
+          width: 100%;
+          height: 100%;
+          border: 2px solid green;
+        }
+
+        .snapshot-btn {
+          position: absolute;
+          bottom: 20px;
+          left: 50%;
+          transform: translateX(-50%);
+          border-radius: 50%;
+          width: 80px;
+          height: 80px;
+          background-color: white;
+          color: white;
+          font-size: 24px;
+          border: none;
+          cursor: pointer;
+          border: 2px solid red;
+        }
+
+        .snapshot-btn__inner {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+          background-color: yellow;
+          border: 5px solid red;
+        }
+
+        @media (max-width: 600px) {
+          .camera-container {
+            flex-direction: column;
+          }
+
+          .video-container,
+          .preview-container {
+            width: 100%;
+            height: 50%;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default ObjectDetection;
+
