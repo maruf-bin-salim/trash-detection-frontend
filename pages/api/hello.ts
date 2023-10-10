3264f0df7bcd002ea5944469d474483139e61948
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-10-11 00:06:54 +0600

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,9 +7,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       // Get the raw image data from the request body
       const imageBuffer = req.body;
+      var buf = Buffer.from(body); 
+      var base64Data = buf.toString('base64') 
+      const dataUrl = 'data:image/jpeg;base64,' + base64Data; 
 
-      // Convert the image buffer to a Data URL
-      const dataUrl = `data:image/jpeg;base64,${imageBuffer.toString('base64')}`;
+      
 
       // Example: Return the Data URL in the response
       return res.status(200).json({ dataUrl });
