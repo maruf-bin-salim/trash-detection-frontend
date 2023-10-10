eaaeca2d6a8e3497c931b56a5314c808e2f3948c
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-10-10 23:43:21 +0600

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// pages/api/uploadImage.js
 
-type Data = {
-  name: string
-}
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      // Get the raw image data from the request body
+      const imageBuffer = req.body;
+
+      // Process the imageBuffer as needed
+      // You can save the image, perform classification, etc.
+      // For example, you can use a library like 'sharp' to process the image:
+      // const sharp = require('sharp');
+      // const processedImage = await sharp(imageBuffer).resize(300, 200).toBuffer();
+
+      // Example: Return a JSON response indicating the image upload was successful
+      return res.status(200).json({ message: 'Image upload successful' });
+    } catch (error) {
+      console.error('Error processing image:', error);
+      return res.status(500).json({ message: 'Error processing image' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 }
